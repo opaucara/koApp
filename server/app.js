6537dd4b1bff62a7bb6cc40d03c62b0a8595e93a
opaucara <oscarlpaucara@hotmail.com>
2013-07-18 23:47:04 +0000

    Adding post it notes

--- server/app.js
+++ server/app.js
@@ -4,3 +4,4 @@ http.createServer(function (req, res) {
   res.end('Hello World\n');
 }).listen(3000, '0.0.0.0');
 console.log('Server rfsdfdunning at http://0.0.0.0:3000/');
+//  mongo --shell --host ds035488.mongolab.com --port 35488 -u admin -p admin nitroussample 
