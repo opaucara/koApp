adab5db34760781057a055f1a419d8ba1173c43c
Oscar Paucara <oscar.paucara@globant.com>
2013-08-08 00:12:34 -0300

    Fix koAccordion rebind on ajax calls

--- public/js/ko.bindingHandler.koAccordion.js
+++ public/js/ko.bindingHandler.koAccordion.js
@@ -4,15 +4,18 @@ define([
   'knockout'
 ], function ($, $ui, ko) {
   ko.bindingHandlers.koAccordion = {
-    init: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-      $(element).accordion(valueAccessor());
-        // This will be called when the binding is first applied to an element
-        // Set up any initial state, event handlers, etc. here
-    },
-    update: function(element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
-        // This will be called once when the binding is first applied to an element,
-        // and again whenever the associated observable changes value.
-        // Update the DOM element based on the supplied values here.
-    }
+   init: function (element, valueAccessor) {
+            var options = valueAccessor(),
+                accordianOptions = options.options;
+            $(element).accordion(accordianOptions);
+        },
+        update: function (element, valueAccessor) {
+            var options = valueAccessor(),
+                accordianOptions = options.options
+                ;
+            var dependency = ko.utils.unwrapObservable(options.updateOn);
+                
+            $(element).accordion('destroy').accordion(accordianOptions);
+        }
   };
 });
--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,26 @@
-require.config({
-  paths: {
-    jquery: 'vendor/jquery-1.7.2.min',
-    jqueryui: 'vendor/jquery-ui.min',
-    knockout: 'vendor/knockout-2.3.0',
-    knockoutMin: 'vendor/knockout-2.3.0.debug',
-    underscore: 'vendor/underscore-min'
-    //bbloader: 'libs/backbone.loader',
-  }
-});
+if (devEnv) {
+  require.config({
+    paths: {
+      jquery: 'vendor/jquery-1.7.2.min',
+      jqueryui: 'vendor/jquery-ui',
+      knockout: 'vendor/knockout-2.3.0.debug',
+      underscore: 'vendor/underscore-min'
+      //bbloader: 'libs/backbone.loader',
+    }
+  });
+}else {
+  require.config({
+    paths: {
+      jquery: 'vendor/jquery-1.7.2.min',
+      jqueryui: 'vendor/jquery-ui.min',
+      knockout: 'vendor/knockout-2.3.0',
+      knockoutMin: 'vendor/knockout-2.3.0.debug',
+      underscore: 'vendor/underscore-min'
+      //bbloader: 'libs/backbone.loader',
+    }
+  });
+}
+
 
 require([
   'deloitteApp',
